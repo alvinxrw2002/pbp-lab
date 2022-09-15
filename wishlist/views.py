c0613e052960dfcb88bb16f7f93776359f53bad6
alvin <91765736+alvinxrw2002@users.noreply.github.com>
2022-09-15 07:36:34 +0700

    Update views.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,12 +1,20 @@
+from django.http import HttpResponse
+from django.core import serializers
 from multiprocessing import context
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
 # Create your views here.
+data_barang_wishlist = BarangWishlist.objects.all()
+context = {
+    'list_barang' : data_barang_wishlist,
+    'nama' : 'Alvin Xavier Rakha Wardhana'
+}
+
 def show_wishlist(request):
-    data_barang_wishlist = BarangWishlist.objects.all()
-    context = {
-        'list_barang' : data_barang_wishlist,
-        'nama' : 'Alvin Xavier Rakha Wardhana'
-    }
     return render(request, "wishlist.html", context)
+
+data = BarangWishlist.objects.all()
+
+def show_xml(request):
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
