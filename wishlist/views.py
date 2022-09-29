abe93636a4028166011210ba6b5482900c0a9df9
alvinxrw <alvin.xavier@ui.ac.id>
2022-09-29 09:38:54 +0700

    tutorial 4 inital commit

--- wishlist/views.py
+++ wishlist/views.py
@@ -16,7 +16,7 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
         'list_barang' : data_barang_wishlist,
-        'nama' : 'Alvin Xavier Rakha Wardhana',
+        'nama' : "Alvin Xavier Rakha Wardhana",
         'last_login': request.COOKIES['last_login']
     }
     return render(request, "wishlist.html", context)
